ee60f656f8df03425d0f3161bc4061d3ecf05090
Vinicius Skonicezny <vinicius.skonicezny@meetkai.com>
2024-01-06 22:33:46 -0300

    feat: hero section basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { Input } from "components/ui/input";
+
+const Hero = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button
-          variant="default"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </Button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <header></header>
+        <div>
+          <Hero />
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/fixtures/README.md
@@ -0,0 +1,3 @@
+# All files were generated using Chatgpt
+
+[https://chat.openai.com/](https://chat.openai.com/)
